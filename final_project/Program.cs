b2600e72d1272a28dd30c65783a957356ca07e3a
Фёдор Березенко <f.berezenko@hotmail.com>
2024-06-13 02:11:31 +0300

    добавил программу, решающую задачу

--- final_project/Program.cs
+++ final_project/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] words_array;
+Console.WriteLine("Введите несколько строк через запятую");
+string words = Console.ReadLine();
+words_array = words.Split(',');
+string[] final_words = new string[words_array.Length];
+int j = 0;
+foreach (var word in words_array)
+{
+    if (word.Length <= 3)
+    {
+        final_words[j] = word;
+        j++;
+    }
+}
+Console.WriteLine(string.Join(", ", final_words, 0, j));
